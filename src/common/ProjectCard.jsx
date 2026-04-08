ead9d1b012ffad8a2c8c412c5b23e80f614b9e7a
Minh <223230201+BlackBeard1720@users.noreply.github.com>
2026-04-08 20:52:52 +0700

    add Contact and Footer

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import "./ProjectCard.css";
 
 function ProjectCard({ src, link, h3, p }) {
   return (
-    <a href={link} target="_blank">
-      <img className="hover" src={src} alt={`${h3} thumnail`} />
-      <h3>{h3}</h3>
-      <p>{p}</p>
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="project-card"
+    >
+      <div className="image-container">
+        <img src={src} alt={h3} className="project-image" />
+      </div>
+
+      <div className="project-content">
+        <h3>{h3}</h3>
+        <p>{p}</p>
+      </div>
     </a>
   );
 }
